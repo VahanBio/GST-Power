5318bae80cc702f4e9334acd6471745f32f3c89b
Vahan Baghdasaryan <vahan.baghdasaryan.99@gmail.com>
2022-04-22 10:59:27 +0400

    outsideclick

--- src/assets/js/modules/_header.js
+++ src/assets/js/modules/_header.js
@@ -1,11 +1,14 @@
 import $ from 'jquery'
 import {gsap} from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {clickOutSide} from "./_utils";
 
 gsap.registerPlugin(ScrollTrigger)
 
 export default class Header {
     constructor() {
+        this.navToggle = $('.nav__toggle')
+        this.dropBtn = $('.dropdown--toggle')
         this.init()
     }
 
@@ -15,16 +18,27 @@ export default class Header {
     }
 
     navMobile() {
-        $(document).on('click', '.nav__toggle', function () {
+        this.navToggle.on('click', function () {
             $(this).toggleClass('nav__toggle--active')
             $('.nav__list').toggleClass('nav__list--open')
-            $('.dropdown--toggle')
+            this.dropBtn
                 .next().slideUp()
                 .parent().removeClass('nav__list--item--open')
+        })
+        clickOutSide({
+            ele: '.nav__list',
+            callback: (ele) => {
+                $(ele).removeClass('nav__list--open')
+                this.navToggle.removeClass('nav__toggle--active')
+                this.dropBtn
+                    .next().slideUp()
+                    .parent().removeClass('nav__list--item--open')
+            }
         })
     }
 
     navDropdowns() {
+
         if ($(window).width() <= 991.8) {
             $(document).on('click', '.dropdown--toggle', function () {
                 const $this = $(this);
@@ -32,10 +46,12 @@ export default class Header {
                 $this.next().slideToggle();
             })
         }
+
         if ($(window).width() >= 992) {
             $('.nav__list--item').each(function () {
-                $(this).find('.dropdown--toggle').on('click', function (e) {
+                $(this).find(this.dropBtn).on('click', function (e) {
                     e.preventDefault()
+                    $(this).parent().addClass('nav__list--active')
                     $('.nav__list').addClass('nav__list--open')
                     $(this).siblings('.nav__list--dropdown').addClass('nav__list--dropdown--open')
                 })
